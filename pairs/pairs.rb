4dbbf63cf70c0767609e6a80eb908b7aa1ad470d
HeleneMartin <helene.martin@fundingcircle.com>
2014-04-04 14:53:59 +0100

    add pairs.rb

---
+++ pairs/pairs.rb
@@ -0,0 +1,15 @@
+pairs = [
+  "Kun Vendell", 
+  "Jaya Lecce",
+  "Sabah Whelan",
+  "Jordan Dimitriou",
+  "Maria Benini",
+  "Karen Cloutier",
+  "Jaswinder Wood",
+  "Eugenia Seward",
+  "Boris Vela",
+
+]
+
+
+puts pairs.sample(2)
